b9800aec5971c1097d3983687bb5a71dc5b8b346
kuba3047 <52176397+kuba3047@users.noreply.github.com>
2021-03-18 18:58:47 +0000

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: './config.env' });
-const express = require('express');
-
-const app = express();
-
-//The port will be displayed in the terminal to let you know what port the server is running on
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
